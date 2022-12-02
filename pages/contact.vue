66ae085842761020f1c552ba8b7b0c839d7953d5
Anticopyright <53624153+dudul-bdul@users.noreply.github.com>
2022-12-02 21:12:00 +0700

    New

---
+++ pages/contact.vue
@@ -0,0 +1,148 @@
+<template>
+  <section>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <SectionsHeroAlt :hero-alt="heroAlt" />
+        <v-container>
+          <v-row class="py-16">
+            <v-col md="5" cols="12">
+              <div class="text-h6 font-weight-bold mb-5">Our Office</div>
+              <v-list two-line class="transparent">
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon color="primary"> mdi-map-marker </v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content>
+                    <v-list-item-title>ADDRESS</v-list-item-title>
+                    <v-list-item-subtitle
+                      >KST Unpad, Bandung, Indonesia</v-list-item-subtitle
+                    >
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon color="primary"> mdi-email </v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content>
+                    <v-list-item-title>EMAIL</v-list-item-title>
+                    <v-list-item-subtitle
+                      >info@yellowearth.co.id</v-list-item-subtitle
+                    >
+                    <v-list-item-subtitle
+                      >support@yellowearth.co.id</v-list-item-subtitle
+                    >
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon color="primary"> mdi-phone </v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content>
+                    <v-list-item-title>PHONE</v-list-item-title>
+                    <v-list-item-subtitle>(022) 123-1234</v-list-item-subtitle>
+                    <v-list-item-subtitle>(022) 123-1234</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+              <div class="text-h6 font-weight-bold mt-12 mb-5">
+                Business Hours
+              </div>
+              <v-list two-line class="transparent">
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon color="primary"> mdi-clock </v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>MONDAY to FRIDAY </v-list-item-title>
+                    <v-list-item-subtitle>9am to 5pm</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon color="primary"> mdi-clock </v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>SATURDAY</v-list-item-title>
+                    <v-list-item-subtitle>9am to 2pm</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-icon>
+                    <v-icon color="primary"> mdi-clock </v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>SUNDAY</v-list-item-title>
+                    <v-list-item-subtitle>cLOSED</v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-col>
+            <v-col md="7" cols="12">
+              <div class="text-h4 font-weight-black mb-8">Contact Us</div>
+              <p class="mb-10">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <contactForm />
+            </v-col>
+          </v-row>
+        </v-container>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.763040674982!2d107.76854441435749!3d-6.918907269640867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68db4bb59c6e37%3A0x39209981dea59649!2sGedung%20Kawasan%20Sains%20dan%20Teknologi%20(KST)%20-%20Unpad!5e0!3m2!1sid!2sid!4v1667474557890!5m2!1sid!2sid"
+          width="100%"
+          height="600"
+          frameborder="0"
+          style="border: 0; display: block"
+          allowfullscreen="true"
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+      </v-col>
+    </v-row>
+  </section>
+</template>
+
+<script>
+import contactForm from '~/components/sections/contactForm.vue'
+export default {
+  components: {
+    contactForm,
+  },
+  data() {
+    return {
+      heroAlt: [
+        {
+          src: 'pexels-andrea-piacquadio-3830745.jpg',
+          heading: ' Contact Us ',
+        },
+      ],
+    }
+  },
+  head() {
+    return {
+      title: 'Contact Us',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Infographic hypotheses influencer user experience Long madel ture gen-z paradigm shift client partner network product seilans solve management influencer analytics leverage virality. incubator seed round massmarket. buyer agile development growth hacking business-to-consumer ecosystem',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+#footer-top {
+  display: none !important;
+}
+</style>
